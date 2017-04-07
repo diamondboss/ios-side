82754631a35a3f9bb46d5a497571538c422d316d
LiYuLong012 <383512208@qq.com>
2017-04-07 16:14:47 +0800

    Initial Commit

---
+++ DiamondBoss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiamondBoss
+//
+//  Created by bonday012 on 17/4/7.
+//  Copyright © 2017年 bonday012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DiamondBoss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiamondBoss
+//
+//  Created by bonday012 on 17/4/7.
+//  Copyright © 2017年 bonday012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
